a44cf68e59aa60f943fb70d664c367b00220a578
Michael Pulido <michaelpulido@Michaels-MacBook-Pro.local>
2021-01-28 19:13:39 -0500

    buttons adding. working except for spread

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ async function getNBA() {
     const responseH2H = response.data.data
     const responseSpread = response2.data.data
     const responseOU = response3.data.data
-    // console.log(responseH2H)
+    console.log(responseH2H)
     // console.log(responseOU)
     // console.log(responseSpread)
 
@@ -26,6 +26,11 @@ async function getNBA() {
   }
 }
 getNBA()
+
+
+
+
+
 
 
 
@@ -49,7 +54,6 @@ function addGames(responseH2H) {
     newGame.insertAdjacentHTML('beforeend', gameData)
   })
 }
-
 let fixTime = (isoDate) => {
   let localDate = new Date(isoDate)
   let dateString = localDate.toString()
@@ -64,6 +68,9 @@ let fixTime = (isoDate) => {
 
 
 
+
+
+
 function addMoneyLine(responseH2H) {
   let moneyLineContainer = document.querySelector('.moneyline-container')
 
@@ -73,38 +80,44 @@ function addMoneyLine(responseH2H) {
     newMoneyLine.classList.add('each-gamble')
 
     let moneyLineData = `
-    <div class="moneyline-spacing">
-      <div class="odds-text">${adjustedOdds((game.sites[0].odds.h2h[1]))}</div> 
+    <div class="moneyline-spacing" id="${adjustedOdds((game.sites[0].odds.h2h[1]))}">
+      <div class="odds-text" id="${adjustedOdds((game.sites[0].odds.h2h[1]))}">${adjustedOdds((game.sites[0].odds.h2h[1]))}</div> 
     </div>
 
-    <div class="moneyline-spacing">
-      <div class="odds-text">${adjustedOdds(game.sites[0].odds.h2h[0])}</div>
+    <div class="moneyline-spacing2" id="${adjustedOdds(game.sites[0].odds.h2h[0])}">
+      <div class="odds-text" id="${adjustedOdds(game.sites[0].odds.h2h[0])}">${adjustedOdds(game.sites[0].odds.h2h[0])}</div>
     </div>
     `
     moneyLineContainer.appendChild(newMoneyLine)
     newMoneyLine.insertAdjacentHTML('beforeend', moneyLineData)
 
+    let moneyLineDiv = document.querySelectorAll('.moneyline-spacing')
+    moneyLineDiv.forEach((moneyLine) => {
+      moneyLine.addEventListener('click', (e) => moneyLineClick(e, adjustedOdds((game.sites[0].odds.h2h[1])), game.teams[1]))
+      // console.log(moneyLineDiv)
+    })
+    let moneyLineDiv2 = document.querySelectorAll('.moneyline-spacing2')
+    moneyLineDiv2.forEach((moneyLine2) => {
+      moneyLine2.addEventListener('click', (e) => moneyLineClick(e, adjustedOdds((game.sites[0].odds.h2h[0])), game.teams[0]))
+      // console.log(moneyLineDiv)
+  })
     })
 }
 
 
-// let moneyLineDiv = document.querySelectorAll('.moneyline-spacing')
-//     moneyLineDiv.forEach((moneyLine) => {
-//       moneyLine.addEventListener('click', moneyLineClick)
-//       console.log(moneyLineDiv)
+let moneyLineClick = (e, moneyLine, team) => {
+  // console.log(e.target.id)
+  let betslip = document.querySelector('.button-container')
+  let newBet = document.createElement('div')
+  if (e.target.id === moneyLine) {
+    newBet = `${moneyLine} ${team} <br>`
+    betslip.insertAdjacentHTML('afterbegin', newBet)
+  }
+}
 
 
-// let moneyLineClick = (responseH2H) => {
-//   let betslip = document.querySelector('.bet-container')
-//   let newBet = `${responseH2H.teams[1]} ${response.H2H.adjustedOdds((sites[0].odds.h2h[1]))}`
-  
-//   console.log(newBet)
-
-//     betslip.append(newBet)
-// }
 
 
-// let newBet = `${game.teams[1]} ${adjustedOdds((game.sites[0].odds.h2h[1]))}`
 
 
 
@@ -117,20 +130,44 @@ function addSpreads(responseSpread) {
     newSpread.classList.add('each-gamble')
 
     let spreadData = `
-    <div class="pointsspread-spacing">
-      <div>${game.sites[0].odds.spreads.points[1]}</div> 
-      <div class="odds-text">${adjustedOdds(game.sites[0].odds.spreads.odds[1])}</div>
+    <div class="pointsspread-spacing" id="${game.sites[0].odds.spreads.points[1]}" id="${adjustedOdds(game.sites[0].odds.spreads.odds[1])}">
+      <div id="${game.sites[0].odds.spreads.points[1]}">${game.sites[0].odds.spreads.points[1]}</div> 
+      <div class="odds-text" id="${adjustedOdds(game.sites[0].odds.spreads.odds[1])}">${adjustedOdds(game.sites[0].odds.spreads.odds[1])}</div>
     </div>
 
-    <div class="pointsspread-spacing">
-      <div>${game.sites[0].odds.spreads.points[0]}</div>
-      <div class="odds-text">${adjustedOdds(game.sites[0].odds.spreads.odds[0])}</div>
+    <div class="pointsspread-spacing2" id="${game.sites[0].odds.spreads.points[0]}" id="${adjustedOdds(game.sites[0].odds.spreads.odds[0])}">
+      <div id="${game.sites[0].odds.spreads.points[0]}">${game.sites[0].odds.spreads.points[0]}</div>
+      <div class="odds-text" id="${adjustedOdds(game.sites[0].odds.spreads.odds[0])}">${adjustedOdds(game.sites[0].odds.spreads.odds[0])}</div>
     </div>
       `
     spreadsContainer.appendChild(newSpread)
     newSpread.insertAdjacentHTML('beforeend', spreadData)
+
+    let spreadDiv = document.querySelectorAll('.pointsspread-spacing')
+    spreadDiv.forEach((spread) => {
+      spread.addEventListener('click', (e) => spreadClick(e, game.sites[0].odds.spreads.points[1], adjustedOdds(game.sites[0].odds.spreads.odds[1]), game.teams[1]))
+    })
+    let spreadDiv2 = document.querySelectorAll('.pointsspread-spacing2')
+    spreadDiv2.forEach((spread2) => {
+      spread2.addEventListener('click', (e) => spreadClick(e, game.sites[0].odds.spreads.points[0], adjustedOdds(game.sites[0].odds.spreads.odds[0]), game.teams[0]))
+    })
   })
 }
+
+
+let spreadClick = (e, spread, odds, team) => {
+  // console.log(e.target.id)
+  let betslip = document.querySelector('.button-container')
+  let newBet = document.createElement('div')
+  if (e.target.id === spread) {
+    newBet = `${spread} ${odds} ${team}<br>`
+    betslip.insertAdjacentHTML('afterbegin', newBet)
+  }
+}
+
+
+
+
 
 
 
@@ -155,8 +192,24 @@ function addTotals(responseOU) {
     `
     totalsContainer.appendChild(newTotals)
     newTotals.insertAdjacentHTML('beforeend', totalsData)
+
+    // let totalDiv = document.querySelectorAll('.totalpoints-spacing')
+    // totalDiv.forEach((total) => {
+    //   total.addEventListener('click', (e) => totalClick(e, game.sites[0].odds.totals.points[0], adjustedOdds(game.sites[0].odds.totals.odds[0]), game.teams[0], game.teams[1]))
+    // })
   })
 }
+
+let totalClick = (e, total, odds, team, team1) => {
+  // console.log(e.target.id)
+  let betslip = document.querySelector('.button-container')
+  let newBet = document.createElement('div')
+  if (e.target.id === total) {
+    newBet = `${total} ${odds} ${team} ${team1}<br>`
+    betslip.insertAdjacentHTML('afterbegin', newBet)
+  }
+}
+
 
 
 let adjustedOdds = (odds) => {
